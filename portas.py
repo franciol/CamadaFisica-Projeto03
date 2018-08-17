5264a2d210de0110e7820dcd2e1fb3210df6e068
Francisco <fran.aveiro@hotmail.com>
2018-08-17 16:40:26 -0300

    Inicio do Projeto 3

---
+++ portas.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  9 18:43:15 2018
+
+@author: francisco
+"""
+
+from serial.tools import list_ports
+print(list_ports.comports())
