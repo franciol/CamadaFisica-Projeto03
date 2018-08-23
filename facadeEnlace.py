cda1f62fecf0f1e2706cea7f60ada8dea31ad6e4
Otofuji <ericfoa@al.insper.edu.br>
2018-08-23 15:02:09 -0300

    Verifica o pacote recebido byte a byte

--- facadeEnlace.py
+++ facadeEnlace.py
@@ -48,12 +48,54 @@ def encapsulate(payload):
     return all
 
 def readHeadNAll(receivedAll):
+    
     head = receivedAll[0:21]
     print("head:  ",head[0:21])
     txLen = fromByteToInt(head[0:5])
     eopSystem = head[5:18]
     stuffByte = head[17:21]
-    return txLen, eopSystem, stuffByte
+
+    sanityCheck = []
+    stuffByteCount = 0
+
+
+    
+    for i in range(22, len(receivedAll)):
+        
+        if receivedAll[i] == stuffByte:
+            sanityCheck.append(receivedAll[i+1:i+13])
+            stuffByteCount += 1
+            i = i+13
+            continue
+
+        elif receivedAll[i:i+13] != eopSystem:
+            sanityCheck.append(receivedAll[i])
+
+        else:
+            break
+
+
+
+    if len(sanityCheck) == txLen:
+        
+        print ("sanityCheck = okay")
+        return sanityCheck, txLen, eopSystem, stuffByte
+
+    else:
+        print ("\n\n ERRO  \n\n HOUVE FALHA NA TRANSMISSÃO. FECHANDO APLICAÇÃO… TENTE NOVAMENTE.")
+        quit()
+
+
+
+
+
+        
+
+
+
+
+
+    
 
 
 
